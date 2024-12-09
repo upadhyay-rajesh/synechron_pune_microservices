abdcafa862e18618807214f7165d4a56882639c5
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2024-12-09 14:58:57 +0530

    first comm

---
+++ synechronepuneconsumer/src/main/java/com/synechronepuneconsumer/controller/MyConsumerController.java
@@ -0,0 +1,50 @@
+package com.synechronepuneconsumer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.synechronepuneconsumer.dto.Employee;
+
+@RestController
+public class MyConsumerController {
+	
+	@Autowired
+	private DiscoveryClient dc;
+	
+	
+	@GetMapping("allconsumer")
+	
+	public List<Employee> getAllEmp(){
+		
+		List<ServiceInstance> sList= dc.getInstances("SYNECHRONEPUNEPRODUCER");
+		
+		System.out.println(sList.size());
+		
+		ServiceInstance myproducerInstance=sList.get(0);
+		 String uri= myproducerInstance.getUri().toString();
+		
+		 System.out.println(uri);
+		
+		RestTemplate rd=new RestTemplate();
+		
+		List<Employee> ll=  rd.getForObject(uri+"/displayall", List.class);
+		System.out.println(ll);
+		return ll;
+		
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ synechronepuneconsumer/src/test/java/com/synechronepuneconsumer/SynechronepuneconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synechronepuneconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynechronepuneconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
