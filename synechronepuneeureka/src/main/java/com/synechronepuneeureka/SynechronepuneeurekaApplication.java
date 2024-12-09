91920b10b928a5f03c05e602596389712521b33e
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2024-12-09 15:00:07 +0530

    xscfvb

---
+++ synechronepuneeureka/src/main/java/com/synechronepuneeureka/SynechronepuneeurekaApplication.java
@@ -0,0 +1,15 @@
+package com.synechronepuneeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SynechronepuneeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynechronepuneeurekaApplication.class, args);
+	}
+
+}
---
+++ synechronepuneeureka/src/test/java/com/synechronepuneeureka/SynechronepuneeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synechronepuneeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynechronepuneeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
