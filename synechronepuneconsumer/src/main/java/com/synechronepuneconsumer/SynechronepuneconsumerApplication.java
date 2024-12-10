18533a1963fce11655273a30fa5b159957f0e63e
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2024-12-10 10:21:57 +0530

    xczx

---
+++ synechronepuneconsumer/src/main/java/com/synechronepuneconsumer/SynechronepuneconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.synechronepuneconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynechronepuneconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynechronepuneconsumerApplication.class, args);
+	}
+
+}
