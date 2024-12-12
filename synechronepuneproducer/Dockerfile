aceba56698d9846bbe6b1f016c37972d75b56f02
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2024-12-12 12:12:20 +0530

    aScdv

---
+++ synechronepuneproducer/Dockerfile
@@ -0,0 +1,5 @@
+from openjdk:17
+EXPOSE 9090
+ADD target/synechronepuneproducer-0.0.1-SNAPSHOT.jar synechronepuneproducer.jar
+ENTRYPOINT ["java","-jar","/synechronepuneproducer.jar"]
+ 
--- synechronepuneproducer/src/main/java/com/synechronepuneproducer/controller/MyController.java
+++ synechronepuneproducer/src/main/java/com/synechronepuneproducer/controller/MyController.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.synechronepuneproducer.entity.Employee;
 import com.synechronepuneproducer.service.MyServiceInterface;
 
 @RestController
+@RequestMapping("/api")
 public class MyController {
 	
 	@Autowired
 	private MyServiceInterface mService;
 	
 	
-	@GetMapping("displayall")
+	@GetMapping("/displayall")
 	public List<Employee> getAll(){
 		return mService.getAllService();
 	}
